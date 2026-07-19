69db828b2aba346629ef0fae6b717e05e5306020
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2026-07-19 22:45:06 +0200

    feat(projen): manage github job through projen (#1)
    * fix(pnpm): fix pnpm version issue
    
    * feat(projen): manage github job through projen
    
    * fix

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,62 @@
-const { BaseProject } = require('@gplassard/projen-extensions');
+const { GithubWorkflow, GitHub } = require('projen/lib/github');
+const { BaseProject, WorkflowActionsX, githubAction } = require('@gplassard/projen-extensions');
 
 const project = new BaseProject({
    name: 'public-images',
+});
+const github = GitHub.of(project);
+
+const job = new GithubWorkflow(github, 'publish-plakar', {
+   name: 'Publish Plakar',
+});
+job.on({
+   workflowDispatch: {},
+   push: {
+      branches: ['main'],
+      paths: ['plakar-tag.txt'],
+   },
+});
+job.addJobs({
+   'build-and-push': {
+      runsOn: 'ubuntu-latest',
+      permissions: {
+         contents: 'read',
+         packages: 'write',
+      },
+      steps: [
+         WorkflowActionsX.checkout({}),
+         {
+            name: 'Read tag from file',
+            id: 'tag',
+            run: 'echo "TAG=$(cat plakar-tag.txt)" >> $GITHUB_OUTPUT',
+         },
+          WorkflowActionsX.checkout({
+            repository: 'PlakarKorp/plakar',
+            ref: '${{ steps.tag.outputs.TAG }}',
+          }),
+         {
+            name: 'Log in to GHCR',
+            uses: 'docker/login-action@v3',
+            with: {
+               registry: 'ghcr.io',
+               username: '${{ github.actor }}',
+               password: '${{ secrets.GITHUB_TOKEN }}',
+            },
+         },
+         {
+            name: 'Build and push Docker image',
+            uses: 'docker/build-push-action@v5',
+            with: {
+               context: '.',
+               file: './Dockerfile',
+               push: true,
+               tags: 
+`'ghcr.io/\${{ github.repository }}/plakar:\${{ steps.tag.outputs.TAG }}',
+'ghcr.io/\${{ github.repository }}/plakar:latest',
+`,
+            },
+         }
+      ]
+   }
 });
 project.synth();
