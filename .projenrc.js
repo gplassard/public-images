5b8bd6f25561a04a1ad47c3124000c400d3560cb
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2026-07-28 00:58:40 +0200

    feat(dockerfile): create custom image based on debian

--- .projenrc.js
+++ .projenrc.js
@@ -26,13 +26,24 @@ job.addJobs({
       steps: [
          WorkflowActionsX.checkout({}),
          {
+            name: 'Get short SHA',
+            id: 'sha',
+            run: 'echo "SHORT_SHA=$(git rev-parse --short HEAD)" >> $GITHUB_OUTPUT',
+         },
+         {
             name: 'Read tag from file',
             id: 'tag',
             run: 'echo "TAG=$(cat plakar-tag.txt)" >> $GITHUB_OUTPUT',
          },
+         {
+            name: 'Extract major version',
+            id: 'major',
+            run: 'echo "MAJOR=$(echo ${{ steps.tag.outputs.TAG }} | sed -E \'s/^(v?[0-9]+).*/\\1/\')" >> $GITHUB_OUTPUT',
+         },
           WorkflowActionsX.checkout({
             repository: 'PlakarKorp/plakar',
             ref: '${{ steps.tag.outputs.TAG }}',
+            path: 'plakar-src',
           }),
          {
             name: 'Log in to GHCR',
@@ -50,10 +61,11 @@ job.addJobs({
                context: '.',
                file: './Dockerfile',
                push: true,
-               tags: 
-`'ghcr.io/\${{ github.repository }}/plakar:\${{ steps.tag.outputs.TAG }}',
-'ghcr.io/\${{ github.repository }}/plakar:latest',
-`,
+               tags: [
+                  'ghcr.io/${{ github.repository }}/plakar:latest',
+                  'ghcr.io/${{ github.repository }}/plakar:${{ steps.major.outputs.MAJOR }}',
+                  'ghcr.io/${{ github.repository }}/plakar:${{ steps.tag.outputs.TAG }}-${{ steps.sha.outputs.SHORT_SHA }}',
+               ],
             },
          }
       ]
